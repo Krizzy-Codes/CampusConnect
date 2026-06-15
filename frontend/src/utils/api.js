bdad9978bd3830ec2899b198856b3459841dab58
Krish <krishnaverma0108@gmail.com>
2026-06-15 13:33:48 +0530

    feat: update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://campusconnect-backend-1616.onrender.com',
 });
 
 // Har request mein token automatically add karo
